0a19c585c75648aab7ce56b75a3a0b7dde110f42
Manh <07mandaos@skola.boras.se>
2024-01-25 21:00:34 +0000

    Push

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
-alert("Hello");
+document.querySelector(".num.nine").addEventListener("click", function(){
+     displayresult += "9";
+     document.querySelector("h2").innerHTML = displayresult;
+})
+
+document.querySelector(".num.two").addEventListener("click", function(){
+    displayresult += "2";
+    document.querySelector("h2").innerHTML = displayresult
+})
+
+document.querySelector(".num.tree").addEventListener("click", function(){
+    displayresult += "3";
+    document.querySelector("h2").innerHTML = displayresult
+})
+
+document.querySelector(".num.one").addEventListener("click", function(){
+    displayresult += "1";
+    document.querySelector("h2").innerHTML = displayresult
+})
+
+document.querySelector(".num.four").addEventListener("click", function(){
+    displayresult += "4";
+    document.querySelector("h2").innerHTML = displayresult
+})
+
+document.querySelector(".num.five").addEventListener("click", function(){
+    displayresult += "5";
+    document.querySelector("h2").innerHTML = displayresult
+})
+
+document.querySelector(".num.six").addEventListener("click", function(){
+    displayresult += "6";
+    document.querySelector("h2").innerHTML = displayresult
+})
+
+document.querySelector(".num.seven").addEventListener("click", function(){
+    displayresult += "7";
+    document.querySelector("h2").innerHTML = displayresult
+})
+
+document.querySelector(".num.eight").addEventListener("click", function(){
+    displayresult += "8";
+    document.querySelector("h2").innerHTML = displayresult
+})
+
+document.querySelector(".num.nine").addEventListener("click", function(){
+    displayresult += "9";
+    document.querySelector("h2").innerHTML = displayresult
+})
+
+document.querySelector(".num.zero").addEventListener("click", function(){
+    displayresult += "0";
+    document.querySelector("h2").innerHTML = displayresult
+})
+
+document.querySelector(".num.comma").addEventListener("click", function(){
+    displayresult += ",";
+    document.querySelector("h2").innerHTML = displayresult
+})
+
+document.querySelector(".num.add").addEventListener("click", function(){
+    displayresult += " ";
+    document.querySelector("h2").innerHTML = displayresult
+})
+ 
+var displayresult = " ";
